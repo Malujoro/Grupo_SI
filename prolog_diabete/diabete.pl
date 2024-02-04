392fb8af4d2dfadc2fd089f7bbff4db3d8f062f9
Malujoro <mateus_rocha_sousa@hotmail.com>
2024-02-04 19:36:37 -0300

    elaborando as regras para analisarem os maiores índices de diabete

--- prolog_diabete/diabete.pl
+++ prolog_diabete/diabete.pl
@@ -1,4 +1,4 @@
-% Paciente Sexo Idade Hipertensão *Cardíaco Fumante *IMC *Hemoglobina *Glicose Diabetes
+%   Paciente Sexo Idade Hipertensão *Cardíaco Fumante *IMC *Hemoglobina *Glicose Diabetes
 paciente(['Maria',       feminino,  37.0, nao, nao, passado,    30.5,   5.7, 100], nao).
 paciente(['Gorete',      feminino,  53.0, nao, nao, passado,    26.37,  4.5, 112], nao).
 paciente(['Pedro',       masculino, 54.0, nao, nao, nunca,      31.86,  6.6, 145], nao).
@@ -20,17 +20,174 @@ paciente(['Heloise',     masculino, 56.0, nao, nao, nunca,      26.78,  4.8, 200
 paciente(['Benício',     masculino, 20.0, nao, nao, passado,    23.04,  5.7, 160], nao).
 paciente(['Paulo',       masculino, 70.0, nao, nao, passado,    15.94,  5.8, 158], nao).
 paciente(['Otávio',      masculino, 30.0, nao, nao, passado,    15.8,   6.2, 90],  nao).
-paciente(['Isis',        feminino,  80.0, nao, nao, nunca,      22.04,  9,0, 209], sim).
+paciente(['Isis',        feminino,  80.0, nao, nao, nunca,      22.04,  9.0, 209], sim).
 paciente(['Francivaldo', masculino, 63.0, nao, sim, passado,    27.32,  6.6, 300], sim).
 paciente(['Juvelino',    masculino, 58.0, nao, nao, passado,    32.38,  6.6, 159], sim).
-paciente(['Maya',        feminino,  43.0, sim, nao, nunca,      34.21,  9,0, 160], sim).
+paciente(['Maya',        feminino,  43.0, sim, nao, nunca,      34.21,  9.0, 160], sim).
 paciente(['Genetíldes',  masculino, 56.0, nao, nao, nunca,      37.59,  4.0, 159], nao).
 paciente(['Waldisney',   masculino, 60.0, nao, nao, passado,    25.4,   4.0, 200], nao).
 paciente(['Reinaldo',    masculino, 27.0, nao, nao, passado,    27.32,  3.5, 100], nao).
 paciente(['Frederico',   masculino, 54.0, nao, nao, passado,    30.41,  5.0, 158], nao).
 
-adicionar_paciente([ConjuntoCaracteristicas], Diabetes).
-editar_paciente([ConjuntoCaracteristicas], Diabetes).
-remover_paciente([ConjuntoCaracteristicas], Diabetes).
-calcular_IMC(Peso, Altura, IMC).
-diagnosticar_diabetes([ConjuntoCaracteristicas], Diabetes).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Regra para montar uma lista com os dados de todos os pacientes
+listaPacientes(Lista) :-
+    findall([[Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabete],
+        paciente([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabete),
+        Lista
+        ).
+
+% Regra para calcular a média de uma lista
+calculaMedia(Lista, Media) :-
+    sumlist(Lista, Soma),
+    length(Lista, Tam),
+    Tam > 0,
+    Media is Soma / Tam.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Regra para contar quantas vezes um "Elemento" aparece na lista
+contaIgual([], _, 0) :- !.
+contaIgual([X | T], Elemento, Resultado) :-
+    contaIgual(T, Elemento, Resultado2),
+    ((X = Elemento, !, Resultado is Resultado2 + 1)
+    ;
+    (!, Resultado is Resultado2)).
+
+% Regra para contar quantos números maiores que o "Elemento" aparecem na lista
+contaMaior([], _, 0) :- !.
+contaMaior([X | T], Elemento, Resultado) :-
+    contaMaior(T, Elemento, Resultado2),
+    ((X > Elemento, !, Resultado is Resultado2 + 1)
+    ;
+    (!, Resultado is Resultado2)).
+
+% Regra para contar quantos números menores que o "Elemento" aparecem na lista
+contaMenor([], _, 0) :- !.
+contaMenor([X | T], Elemento, Resultado) :-
+    contaMenor(T, Elemento, Resultado2),
+    ((X < Elemento, !, Resultado is Resultado2 + 1)
+    ;
+    (!, Resultado is Resultado2)).
+
+% Regra para contar quantas vezes um "Elemento" aparece nos pacientes diagnosticados
+contaIgualDiabetico([], [], _, 0) :- !.
+contaIgualDiabetico([X | T], [X2 | T2], Elemento, Resultado) :-
+    contaIgualDiabetico(T, T2, Elemento, Resultado2),
+    ((X2 = sim, X = Elemento, !, Resultado is Resultado2 + 1)
+    ;
+    (!, Resultado is Resultado2)).
+
+% Regra para contar quantos números maiores que o "Elemento" aparecem nos pacientes diagnosticados
+contaMaiorDiabetico([], [], _, 0) :- !.
+contaMaiorDiabetico([X | T], [X2 | T2], Elemento, Resultado) :-
+    contaMaiorDiabetico(T, T2, Elemento, Resultado2),
+    ((X2 = sim, X > Elemento, !, Resultado is Resultado2 + 1)
+    ;
+    (!, Resultado is Resultado2)).
+
+% Regra para contar quantos números menores que o "Elemento" aparecem nos pacientes diagnosticados
+contaMenorDiabetico([], [], _, 0) :- !.
+contaMenorDiabetico([X | T], [X2 | T2], Elemento, Resultado) :-
+    contaMenorDiabetico(T, T2, Elemento, Resultado2),
+    ((X2 = sim, X < Elemento, !, Resultado is Resultado2 + 1)
+    ;
+    (!, Resultado is Resultado2)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+% Regra para "Montar" uma lista com o sexo de todos os pacientes
+listaSexo([], []) :- !.
+listaSexo([[[_, Sexo, _, _, _, _, _, _, _], _] | T], [Sexo | T2]) :- 
+    listaSexo(T, T2).
+
+% Regra para "Montar" uma lista com as Idades de todos os pacientes
+listaIdade([], []) :- !.
+listaIdade([[[_, _, Idade, _, _, _, _, _, _], _] | T], [Idade | T2]) :- 
+    listaIdade(T, T2).
+
+% Regra para "Montar" uma lista com a existência de Hipertensão de todos os pacientes
+listaHipertensao([], []) :- !.
+listaHipertensao([[[_, _, _, Hipertensao, _, _, _, _, _], _] | T], [Hipertensao | T2]) :- 
+    listaHipertensao(T, T2).
+
+% Regra para "Montar" uma lista com os IMCs de todos os pacientes
+listaIMC([], []) :- !.
+listaIMC([[[_, _, _, _, _, _, IMC, _, _], _] | T], [IMC | T2]) :- 
+    listaIMC(T, T2).
+
+% Regra para "Montar" uma lista com o índice de Hemoglobina de todos os pacientes
+listaHemoglobina([], []) :- !.
+listaHemoglobina([[[_, _, _, _, _, _, _, Hemoglobina, _], _] | T], [Hemoglobina | T2]) :- 
+    listaHemoglobina(T, T2).
+
+% Regra para "Montar" uma lista com o índice de Glicose de todos os pacientes
+listaGlicose([], []) :- !.
+listaGlicose([[[_, _, _, _, _, _, _, _, Glicose], _] | T], [Glicose | T2]) :- 
+    listaGlicose(T, T2).
+
+% Regra para "Montar" uma lista com a "posição" dos diabéticos
+listaDiabete([], []) :- !.
+listaDiabete([[_, Diabete] | T], [Diabete | T2]) :- 
+    listaDiabete(T, T2).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% sintomas([Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose])
+% Serão calculados os "sintomas" ou fatores com mais chances de indicar diabetes.
+% Será levado em consideração o fator com maior porcentagem de ocorrência.
+% Por exemplo: [Feminino - 8/15 - 53,3% | Masculino - 4/14 - 28,5%], então aparentemente pessoas do gênero feminino têm mais chances de diabetes
+
+    sintomas([Sexo, Hipertensao]) :-
+    listaPacientes(Lista),
+    listaDiabete(Lista, ListaDiabete),
+
+    % Será calculado a quantidade de gente do sexo feminino e masculino cadastrado no sistema, bem como a porcentagem de diabéticos por gênero
+    listaSexo(Lista, ListaSexo),
+    contaIgual(ListaSexo, feminino, QuantFeminino),
+    contaIgual(ListaSexo, masculino, QuantMasculino),
+    contaIgualDiabetico(ListaSexo, ListaDiabete, feminino, QuantFeminino2),
+    contaIgualDiabetico(ListaSexo, ListaDiabete, masculino, QuantMasculino2),
+    write('Mulheres: '), write(QuantFeminino2), write('/'), write(QuantFeminino), nl,
+    write('Homens: '), write(QuantMasculino2), write('/'), write(QuantMasculino), nl,
+    ((QuantFeminino2/QuantFeminino >= QuantMasculino2/QuantMasculino, !, Sexo = feminino);(Sexo = masculino)),
+
+    % Será calculado a quantidade de gente com hipertensão (ou não) cadastrado no sistema, bem como a porcentagem de diabéticos
+    listaHipertensao(Lista, ListaHipertensao),
+    contaIgual(ListaHipertensao, sim, SimHipertensao),
+    contaIgual(ListaHipertensao, nao, NaoHipertensao),
+    contaIgualDiabetico(ListaHipertensao, ListaDiabete, sim, SimHipertensao2),
+    contaIgualDiabetico(ListaHipertensao, ListaDiabete, nao, NaoHipertensao2),
+    write('Sim: '), write(SimHipertensao2), write('/'), write(SimHipertensao), nl,
+    write('Não: '), write(NaoHipertensao2), write('/'), write(NaoHipertensao), nl,
+    ((SimHipertensao2/SimHipertensao >= NaoHipertensao2/NaoHipertensao, !, Hipertensao = sim);(Hipertensao = nao)).
+
+
+main() :-
+    listaPacientes(Lista),
+    
+    listaDiabete(Lista, Diabete),
+    write(Diabete), nl.
+    % listaIdade(Lista, Idade),
+    % calculaMedia(Idade, MediaIdade),
+    
+    % listaIMC(Lista, IMC),
+    % calculaMedia(IMC, MediaIMC),
+
+    % listaHemoglobina(Lista, Hemoglobina),
+    % calculaMedia(Hemoglobina, MediaHemo),
+
+    % listaGlicose(Lista, Glicose),
+    % calculaMedia(Glicose, MediaGlico).
+
+% adicionar_paciente([ConjuntoCaracteristicas], Diabetes).
+
+% editar_paciente([ConjuntoCaracteristicas], Diabetes).
+
+% remover_paciente([ConjuntoCaracteristicas], Diabetes).
+
+% Regra para calcular o IMC de um paciente
+calcular_IMC(Peso, Altura, IMC) :- 
+    Peso > 0, Altura > 0, IMC is Peso / (Altura * Altura).
+
+% diagnosticar_diabetes([ConjuntoCaracteristicas], Diabetes).
